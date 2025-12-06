11e33a382fcbfa48a74ff2ef75e94cde7cc98351
Galaceon <antoniogarcialtr@gmail.com>
2025-12-06 19:34:42 +0100

    Creating Users Table and creating alerts for register validation

--- views/auth/registro.php
+++ views/auth/registro.php
@@ -2,31 +2,37 @@
     <h2 class="auth__heading"><?php echo $titulo; ?></h2>
     <p class="auth__texto">Regístrate en DevWebCamp</p>
 
-    <div class="formulario__campo">
-        <label for="nombre" class="formulario__label">Nombre</label>
-        <input type="text" name="nombre" id="nombre" placeholder="Tu nombre" class="formulario__input">
-    </div>
+    <?php
+        require_once __DIR__ . '/../templates/alertas.php';
+    ?>
 
-    <div class="formulario__campo">
-        <label for="apellido" class="formulario__label">Apellido</label>
-        <input type="text" name="apellido" id="apellido" placeholder="Tu apellido" class="formulario__input">
-    </div>
+    <form action="/registro" class="formulario" method="POST">
+        <div class="formulario__campo">
+            <label for="nombre" class="formulario__label">Nombre</label>
+            <input type="text" name="nombre" id="nombre" placeholder="Tu nombre" class="formulario__input" value="<?php echo $usuario->nombre;?>">
+        </div>
 
-    <div class="formulario__campo">
-        <label for="email" class="formulario__label">Email</label>
-        <input type="email" name="email" id="email" placeholder="Tu email" class="formulario__input">
-    </div>
+        <div class="formulario__campo">
+            <label for="apellido" class="formulario__label">Apellido</label>
+            <input type="text" name="apellido" id="apellido" placeholder="Tu apellido" class="formulario__input" value="<?php echo $usuario->apellido;?>">
+        </div>
 
-    <div class="formulario__campo">
-        <label for="password" class="formulario__label">Password:</label>
-        <input type="password" name="password" id="password" placeholder="Tu password" class="formulario__input">
-    </div>
-    <div class="formulario__campo">
-        <label for="password2" class="formulario__label">Repetir Password:</label>
-        <input type="password" name="password2" id="password2" placeholder="Repetir password" class="formulario__input">
-    </div>
+        <div class="formulario__campo">
+            <label for="email" class="formulario__label">Email</label>
+            <input type="email" name="email" id="email" placeholder="Tu email" class="formulario__input" value="<?php echo $usuario->email;?>">
+        </div>
 
-    <input type="submit" class="formulario__submit" value="Crear Cuenta">
+        <div class="formulario__campo">
+            <label for="password" class="formulario__label">Password:</label>
+            <input type="password" name="password" id="password" placeholder="Tu password" class="formulario__input">
+        </div>
+        <div class="formulario__campo">
+            <label for="password2" class="formulario__label">Repetir Password:</label>
+            <input type="password" name="password2" id="password2" placeholder="Repetir password" class="formulario__input">
+        </div>
+
+        <input type="submit" class="formulario__submit" value="Crear Cuenta">
+    </form>
 
     <div class="acciones">
         <a href="/login" class="acciones__enlace">Iniciar Sesión</a>
