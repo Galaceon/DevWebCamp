0e93805f982a38f0d7590429deec82c044db26ac
Galaceon <antoniogarcialtr@gmail.com>
2025-12-14 07:46:07 +0100

    Creating Speakers and Storing the Image Path

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -30,12 +30,14 @@ class PonentesController {
                 }
 
                 $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('png', 80);
-                $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('webp$imagen_webp', 80);
+                $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('webp', 80);
 
                 $nombre_imagen = md5(uniqid(rand(), true));
 
                 $_POST['imagen'] = $nombre_imagen;
             }
+
+            $_POST['redes'] = json_encode($_POST['redes'], JSON_UNESCAPED_SLASHES);
 
             $ponente->sincronizar($_POST);
 
@@ -43,6 +45,19 @@ class PonentesController {
             $alertas = $ponente->validar();
 
             // Guardar el Registro
+            if(empty($alertas)) {
+
+                // Guardar las imagenes
+                $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . ".png");
+                $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . ".webp");
+
+                // Guardar en la DB
+                $resultado = $ponente->guardar();
+
+                if($resultado) {
+                    header('Location: /admin/ponentes');
+                }
+            }
         }
 
         $router->render('admin/ponentes/crear', [
