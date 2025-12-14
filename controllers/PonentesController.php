16b7ae0a70b7458c0dd050955cdfc7f351340ed8
Galaceon <antoniogarcialtr@gmail.com>
2025-12-14 07:00:51 +0100

    read ponente image and trigger PNG and WebP version

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Intervention\Image\ImageManagerStatic as Image;
 use Model\Ponente;
 use MVC\Router;
 
@@ -20,10 +21,28 @@ class PonentesController {
         $ponente =  new Ponente;
 
         if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Leer Imagen
+            if(!empty($_FILES['imagen']['tmp_name'])) {
+                $carpeta_imagenes = '../public/img/speakers';
+
+                if(!is_dir($carpeta_imagenes)) {
+                    mkdir($carpeta_imagenes, 0755, true);
+                }
+
+                $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('png', 80);
+                $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('webp$imagen_webp', 80);
+
+                $nombre_imagen = md5(uniqid(rand(), true));
+
+                $_POST['imagen'] = $nombre_imagen;
+            }
+
             $ponente->sincronizar($_POST);
 
             // Validar
             $alertas = $ponente->validar();
+
+            // Guardar el Registro
         }
 
         $router->render('admin/ponentes/crear', [
