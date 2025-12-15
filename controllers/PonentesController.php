fbe212a521c8d63fe8281e5d24171ef30b368e0b
Galaceon <antoniogarcialtr@gmail.com>
2025-12-15 07:58:38 +0100

    delete ponentes

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -140,4 +140,24 @@ class PonentesController {
             'redes' => $redes
         ]);
     }
+
+    public static function eliminar() {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_POST['id'];
+
+            $ponente = Ponente::find($id);
+
+            if(!isset($ponente)) {
+                header('Location: /admin/ponentes');
+            }
+
+            $resultado  = $ponente->eliminar();
+
+            if($resultado) {
+                header('Location: /admin/ponentes');
+            }
+
+            debuguear($ponente);
+        }
+    }
 }
