e14193a46e213bcb10ab1c6c77f304809541290e
Galaceon <antoniogarcialtr@gmail.com>
2025-12-19 20:24:12 +0100

    Creating a new method in ActiveRecord to search values in two columns

--- controllers/APIEventos.php
+++ controllers/APIEventos.php
@@ -16,9 +16,12 @@ class APIEventos {
 
         if(!$dia_id || !$categoria_id) {
             echo json_encode([]);
+            return;
         }
 
         // Consultar DB
-        $eventos = EventoHorario::
+        $eventos = EventoHorario::whereArray(['dia_id' => $dia_id, 'categoria_id' => $categoria_id]);
+
+        echo json_encode($eventos);
     }
 }
