3f12d49e276ce649ccfed777aa106e7ffbe2d031
Galaceon <antoniogarcialtr@gmail.com>
2025-12-22 07:21:53 +0100

    filter ponentes searching them, needed to create events page

--- src/js/ponentes.js
+++ src/js/ponentes.js
@@ -7,6 +7,8 @@
 
         obtenerPonentes();
 
+        ponentesInput.addEventListener('input', buscarPonentes)
+
         async function obtenerPonentes() {
             const url = `/api/ponentes`;
 
@@ -23,8 +25,21 @@
                     id: `${ponente.id}`
                 }
             })
+        }
 
-            console.log(ponentes);
+        function buscarPonentes(e) {
+            const busqueda = e.target.value;
+
+            if(busqueda.length > 3) {
+                 const expresion = new RegExp(busqueda, 'i');
+                 ponentesFiltrados = ponentes.filter(ponente => {
+                    if(ponente.nombre.toLowerCase().search(expresion) != -1) {
+                        return ponente;
+                    }
+                 })
+
+                 console.log(ponentesFiltrados);
+            }
         }
     }
 })();
