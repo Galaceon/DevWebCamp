1a54b8ab443a49246419fd4fa9bf8db693afa544
Galaceon <antoniogarcialtr@gmail.com>
2025-12-23 20:23:09 +0100

    creating devwebcamp(aboutUs) public page

---
+++ views/paginas/devwebcamp.php
@@ -0,0 +1,25 @@
+<main class="devwebcamp">
+    <h2 class="devwebcamo__heading"><?php echo $titulo; ?></h2>
+    <p class="devwebcamp__descripcion">Conoce la conferencia mas importante de España</p>
+
+    <div class="devwebcamp__grid">
+        <div class="devwebcamp__imagen">
+            <picture>
+                <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
+                <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
+                <img loading="lazy" width="200" height="300" src="build/img/sobre_devwebcamp.jpg" alt="Imagen DevWebCamp">
+            </picture>
+        </div>
+
+        <div class="devwebcamp__contenido">
+            <p class="devwebcamp__texto">DevWebCamp es una conferencia anual dedicada al desarrollo web y móvil, donde profesionales de referencia se reúnen para
+            compartir conocimientos, experiencias reales y las últimas tendencias del sector. Durante varios días, asistentes de todo 
+            el país disfrutan de ponencias inspiradoras y workshops prácticos enfocados en tecnologías modernas.</p>
+
+            <p class="devwebcamp__texto">El evento se celebra en España y ofrece diferentes tipos de pases adaptados a cada perfil, permitiendo la asistencia 
+            presencial, online o gratuita. Cada modalidad brinda acceso a contenidos de alto valor impartidos por expertos con amplia
+            trayectoria profesional, fomentando el aprendizaje continuo.</p>
+
+        </div>
+    </div>
+</main>
