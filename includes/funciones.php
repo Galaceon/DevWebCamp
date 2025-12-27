f1e0ec006ec65a07aaa28fd30b6b21dbade1e8d8
Galaceon <antoniogarcialtr@gmail.com>
2025-12-27 23:58:32 +0100

    creating the block with the summary and amounts

--- includes/funciones.php
+++ includes/funciones.php
@@ -1,25 +1,33 @@
 <?php
 
+// Muestra información estructurada de una variable
 function debuguear($variable) : string {
     echo "<pre>";
     var_dump($variable);
     echo "</pre>";
     exit;
 }
+
+// Escapa el HTML especial para evitar ataques XSS
 function s($html) : string {
     $s = htmlspecialchars($html);
     return $s;
 }
+
+// Comprueba que la página actual(url) sea igual a la pasada como parámetro en $path
 function pagina_actual($path) {
-    return str_contains($_SERVER['PATH_INFO'], $path) ? true : false;
+    return isset($_SERVER['PATH_INFO']) && str_contains($_SERVER['PATH_INFO'], $path);
 }
 
+// Verifica si el usuario está autenticado
 function is_auth() : bool {
     if(!isset($_SESSION)) {
         session_start();
     }
     return isset($_SESSION['nombre']) && !empty($_SESSION);
 }
+
+// Verifica si el usuario es administrador
 function is_admin() {
     if(!isset($_SESSION)) {
         session_start();
