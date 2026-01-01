220e586b4a032cb7a848bd45a08d471e4117641b
Galaceon <antoniogarcialtr@gmail.com>
2026-01-01 17:43:56 +0100

    docs for tags.js

--- src/js/tags.js
+++ src/js/tags.js
@@ -1,14 +1,14 @@
 (function() {
-    const tagsInput = document.querySelector('#tags_input');
+    const tagsInput = document.querySelector('#tags_input'); // Input donde se escriben los tags
 
     if(tagsInput) {
 
-        const tagsDiv = document.querySelector('#tags');
-        const tagsInputHidden = document.querySelector('[name="tags"]');
+        const tagsDiv = document.querySelector('#tags'); // Div donde se muestran los tags añadidos
+        const tagsInputHidden = document.querySelector('[name="tags"]'); // Input oculto para enviar los tags al backend
 
-        let tags = [];
+        let tags = []; // Array para almacenar los tags añadidos
 
-        // Recuperar el input oculto
+        // Recuperar el input oculto si ya hay tags añadidos (edicion de evento)
         if(tagsInputHidden.value !== '') {
             tags = tagsInputHidden.value.split(',');
             mostrarTags();
@@ -18,6 +18,7 @@
         tagsInput.addEventListener('keypress', guardarTag);
 
         function guardarTag(e) {
+            // Si el usuario presiona la coma (,) se guarda el tag
             if(e.keyCode === 44) {
 
                 // Si hay escritos espacios el codigo no se ejecuta
@@ -31,15 +32,19 @@
                 // Nuevo array tags para no mutar el anterior y añadimos el contenido nuevo
                 tags = [...tags, e.target.value.trim()];
 
-                // vaciamos el input type="text" para que este vacio en su uso posterior
+                // Vaciamos el input type="text" para que este vacio en su uso posterior
                 tagsInput.value = '';
 
+                // Al añadir un nuevo tag al array de tags, mostrar los tags actualizados
                 mostrarTags();
             }
         }
 
+        // Mostrar los tags en el HTML
         function mostrarTags() {
-            tagsDiv.textContent = '';
+            tagsDiv.textContent = ''; // Limpiar el HTML previo si hay tags
+
+            // Recorrer el array de tags y mostrarlos en el HTML
             tags.forEach(tag => {
                 const etiqueta = document.createElement('LI');
                 etiqueta.classList.add('formulario__tag');
@@ -49,16 +54,20 @@
                 tagsDiv.appendChild(etiqueta);
             })
 
+            // Actualizar el input hidden con los tags actuales
             actualizarInputHidden();
         }
 
+        // Eliminar un tag al hacer click en el mismo
         function eliminarTag(e) {
             e.target.remove();
+            // Filtrar el array de tags para eliminar el tag clickeado (filtar por el tag que no tenga el mismo texto)
             tags = tags.filter(tag => tag !== e.target.textContent);
             actualizarInputHidden();
         }
 
         function actualizarInputHidden() {
+            // Convertir el array de tags a string separado por comas y asignarlo al input hidden para enviarlo al backend
             tagsInputHidden.value = tags.toString();
         }
     }
