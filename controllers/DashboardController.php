e13ef087f4bd885ce08a64833a4b7f021723f7c7
Galaceon <antoniogarcialtr@gmail.com>
2026-01-06 22:18:57 +0100

    creating an income block for admin dashboard

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -21,9 +21,16 @@ class DashboardController {
             $registro->usuario = Usuario::find($registro->usuario_id);
         }
 
+        // Calcular los ingresos
+        $virtuales = Registro:: total('paquete_id', 2);
+        $presenciales = Registro:: total('paquete_id', 1);
+
+        $ingresos = ($virtuales * 46.98) + ($presenciales * 191.81);
+
         $router->render('admin/dashboard/index', [
             'titulo' => 'Panel de Administración',
-            'registros' => $registros
+            'registros' => $registros,
+            'ingresos' => $ingresos
         ]);
     }
 }
